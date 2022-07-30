b0114d7530c36bebac9e5735922209c06bafc322
Arzivre <sonyfauzi@outlook.co.id>
2022-07-30 21:27:01 +0700

    feat:new email template

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,58 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { readFileSync } from 'fs'
+import Handlebars from 'handlebars'
+import NextAuth, { User, type NextAuthOptions } from 'next-auth'
+import EmailProvider from 'next-auth/providers/email'
+import GoogleProvider from 'next-auth/providers/google'
+import nodemailer from 'nodemailer'
+import path from 'path'
 import { prisma } from 'src/server/prisma'
+
+const TRANSPORTER = nodemailer.createTransport(process.env.EMAIL_SERVER)
+const EMAIL_DIR = path.resolve(process.cwd(), 'src/emails')
+
+interface Props {
+  identifier: string
+  url: string
+}
+const sendVerificationRequest = ({ identifier, url }: Props) => {
+  const emailFile = readFileSync(path.join(EMAIL_DIR, 'confirm-email.html'), {
+    encoding: 'utf8',
+  })
+  const emailTemplate = Handlebars.compile(emailFile)
+  TRANSPORTER.sendMail({
+    from: `"Signal SOS" ${process.env.EMAIL_FROM}`,
+    to: identifier,
+    subject: 'Your sign-in link for Signal SOS',
+    html: emailTemplate({
+      base_url: process.env.NEXTAUTH_URL,
+      signin_url: url,
+      email: identifier,
+    }),
+  })
+}
+
+const sendWelcomeEmail = async ({ user }: { user: User }) => {
+  const { email } = user
+
+  try {
+    const emailFile = readFileSync(path.join(EMAIL_DIR, 'welcome.html'), {
+      encoding: 'utf8',
+    })
+    const emailTemplate = Handlebars.compile(emailFile)
+    await TRANSPORTER.sendMail({
+      from: `"Signal SOS" ${process.env.EMAIL_FROM}`,
+      to: email!,
+      subject: 'Welcome to Signal SOS!',
+      html: emailTemplate({
+        base_url: process.env.NEXTAUTH_URL,
+        support_email: 'support@signal-sos.vercel.app',
+      }),
+    })
+  } catch (error) {
+    console.log(`❌ Unable to send welcome email to user (${email})`)
+  }
+}
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -12,16 +62,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-      maxAge: 60 * 60 * 24, // Magic links are valid for 1 day
+      maxAge: 60 * 60 * 24,
+      sendVerificationRequest,
     }),
   ],
   callbacks: {
@@ -32,6 +74,8 @@ export const authOptions: NextAuthOptions = {
       return session
     },
   },
+  secret: process.env.NEXTAUTH_SECRET,
+  events: { createUser: sendWelcomeEmail },
 }
 
 export default NextAuth(authOptions)
