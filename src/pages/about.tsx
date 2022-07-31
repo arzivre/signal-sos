368434df4ef7c90fbad0e8a7a537e9291d25457a
Arzivre <sonyfauzi@outlook.co.id>
2022-07-31 22:04:58 +0700

    update about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,16 +13,18 @@ const AboutPage = () => {
       <Layout>
         <main className='container mx-auto my-20 max-w-7xl px-4 text-2xl text-gray-50'>
           <h1 className='mb-8 font-serif text-[calc(1em+8vh)]'>About</h1>
-          <p>natural disaster frenquently happeing</p>
-          <p>
-            it would be esasier if people can get information or connect to
-            other so they can go where safe place, and people know where they
-            need to go
+          <p className='text-gray-400'>
+            Natural disaster frequently happening it would be easier if people
+            can get information or connect to other so they can go where
+            safeplace, and team rescue know where they need to go for fetch
+            people need help
           </p>
-          <p>
-            This app is made to connect comunity when something like natural
-            disaster happen, any individual need help from people create signal
-            so people know where to go
+          <br />
+          <p className='text-gray-400'>
+            This app is made to connect community when something like natural
+            disaster happen, people who need help can create signal SOS, and
+            people who provide help can create signal SHELTER so people know
+            where to get help
           </p>
         </main>
       </Layout>
